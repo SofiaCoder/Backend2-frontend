8c1480a902ddd58ac191730ad8ef8eb01d5642a6
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-26 17:08:32 +0200

    Created structure for MainPage

---
+++ gruppuppgift/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { PostContainer } from "./PostContainer/PostContainer";
+import { UserSearch } from "./UserSearch/UserSearch";
+
+export function MainPage(){
+
+  const [posts, setPosts] = useState([]);
+  return (<div>
+    <h1>Flow</h1>
+    <UserSearch/>
+    <PostContainer posts={posts}/>
+  </div>)
+}
