7872945c41f08936a368271859157992554d8ae8
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-26 17:15:36 +0200

    Added mock data to MainPage

--- gruppuppgift/src/pages/MainPage/MainPage.jsx
+++ gruppuppgift/src/pages/MainPage/MainPage.jsx
@@ -4,7 +4,7 @@ import { UserSearch } from "./UserSearch/UserSearch";
 
 export function MainPage(){
 
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([{username: 'Metin', post: 'My test post', date: new Date().toDateString()}]);
   return (<div>
     <h1>Flow</h1>
     <UserSearch/>
