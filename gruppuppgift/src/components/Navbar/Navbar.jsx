340e8950a23028283bc2f35c3816d4644210bd51
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-27 19:39:19 +0200

    Added Navbar for userpage

--- gruppuppgift/src/components/Navbar/Navbar.jsx
+++ gruppuppgift/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ export function Navbar(){
   const loggedinUser = localStorage.getItem('loggedinUser');
   return <nav>
     <Link to='/mainpage'>Home</Link>
+    <Link to='/userpage'>Users</Link>
     <Link to={`/profile/${loggedinUser}`}>My Profile</Link>
   </nav>
 }
