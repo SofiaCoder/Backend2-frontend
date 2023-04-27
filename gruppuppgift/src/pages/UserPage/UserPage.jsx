7cb825530a1b6ebe762a272de03d02bbd0c40442
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-27 21:04:07 +0200

    Created a user page

---
+++ gruppuppgift/src/pages/UserPage/UserPage.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { getAllUsers } from "./getAllUsers";
+import { UserComponent } from "./UserComponent/UserComponent";
+import { getUser } from "../ProfilePage/getUser";
+
+export function UserPage() {
+  const loggedinUser = localStorage.getItem('loggedinUser');
+
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    async function getStates(){
+      const fetchedUsers = await getAllUsers();
+      const fetchedUser = await getUser(loggedinUser);
+      setCurrentUser(fetchedUser);
+      const usersWithoutCurrentUser = fetchedUsers.filter(user => user.username !== loggedinUser);
+      setUsers(usersWithoutCurrentUser);
+    }
+    getStates();
+  }, []);
+
+  return <>
+  <Navbar/>
+  {users.length > 0 ? users.map((user) => (<UserComponent user={user} key={user.username} currentUser={currentUser}/>)) : <>There are no registered users</>}
+  </>
+}
