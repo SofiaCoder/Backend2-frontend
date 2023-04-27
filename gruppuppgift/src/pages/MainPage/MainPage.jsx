50b16a4bce14594837f83fdc1e9d0d4d17a44ee2
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-27 21:02:59 +0200

    Removed User Search from MainPage

--- gruppuppgift/src/pages/MainPage/MainPage.jsx
+++ gruppuppgift/src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,11 @@
 import { Navbar } from "../../components/Navbar/Navbar";
 import { PostContainer } from "./PostContainer/PostContainer";
-import { UserSearch } from "./UserSearch/UserSearch";
 
 export function MainPage(){
 
   return (<div>
     <Navbar/>
     <h1>Flow</h1>
-    <UserSearch/>
     <PostContainer/>
   </div>)
 }
