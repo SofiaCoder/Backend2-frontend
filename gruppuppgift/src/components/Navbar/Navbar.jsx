5222214e5da47e090f0d851913753d0e87228004
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-27 15:42:55 +0200

    Created a navbar with "home" and "profile" buttons

---
+++ gruppuppgift/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+export function Navbar(){
+  const loggedinUser = localStorage.getItem('loggedinUser');
+  return <nav>
+    <Link to='/mainpage'>Home</Link>
+    <Link to={`/profile/${loggedinUser}`}>My Profile</Link>
+  </nav>
+}
