3ac42d19c1318d8a28c1fde5793bea1de43ff172
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-27 21:17:38 +0200

    Added styling to UserPage

--- gruppuppgift/src/pages/UserPage/UserPage.jsx
+++ gruppuppgift/src/pages/UserPage/UserPage.jsx
@@ -3,26 +3,35 @@ import { Navbar } from "../../components/Navbar/Navbar";
 import { getAllUsers } from "./getAllUsers";
 import { UserComponent } from "./UserComponent/UserComponent";
 import { getUser } from "../ProfilePage/getUser";
+import "./userPage.css";
 
 export function UserPage() {
-  const loggedinUser = localStorage.getItem('loggedinUser');
+    const loggedinUser = localStorage.getItem("loggedinUser");
 
-  const [users, setUsers] = useState([]);
-  const [currentUser, setCurrentUser] = useState({});
+    const [users, setUsers] = useState([]);
+    const [currentUser, setCurrentUser] = useState({});
 
-  useEffect(() => {
-    async function getStates(){
-      const fetchedUsers = await getAllUsers();
-      const fetchedUser = await getUser(loggedinUser);
-      setCurrentUser(fetchedUser);
-      const usersWithoutCurrentUser = fetchedUsers.filter(user => user.username !== loggedinUser);
-      setUsers(usersWithoutCurrentUser);
-    }
-    getStates();
-  }, []);
+    useEffect(() => {
+        async function getStates() {
+            const fetchedUsers = await getAllUsers();
+            const fetchedUser = await getUser(loggedinUser);
+            setCurrentUser(fetchedUser);
+            const usersWithoutCurrentUser = fetchedUsers.filter((user) => user.username !== loggedinUser);
+            setUsers(usersWithoutCurrentUser);
+        }
+        getStates();
+    }, []);
 
-  return <>
-  <Navbar/>
-  {users.length > 0 ? users.map((user) => (<UserComponent user={user} key={user.username} currentUser={currentUser}/>)) : <>There are no registered users</>}
-  </>
+    return (
+        <>
+            <Navbar />
+            <div className="userContainer">
+                {users.length > 0 ? (
+                    users.map((user) => <UserComponent user={user} key={user.username} currentUser={currentUser} />)
+                ) : (
+                    <>There are no registered users</>
+                )}
+            </div>
+        </>
+    );
 }
