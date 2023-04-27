899948e1f5097618f65e7ea564a22096b207e336
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-27 19:04:48 +0200

    Created a Profile Page

---
+++ gruppuppgift/src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { getUser } from "./getUser";
+import { PostComponent } from "../MainPage/PostContainer/PostComponent/PostComponent";
+import { NewPost } from "./NewPost/NewPost";
+
+export function ProfilePage(){
+  const {username} = useParams();
+  const loggedinUser = localStorage.getItem('loggedinUser');
+  const isMyPage = username === loggedinUser;
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getAndSetUser() {
+      const {posts} = await getUser(username);
+      setPosts(posts);
+    }
+    getAndSetUser();
+  }, [username]);
+
+  return <div>
+    <h1>{username}'s profile</h1>
+    {isMyPage && <NewPost posts={posts} setPosts={setPosts}/>}
+    <h3>Posts:</h3>
+    {posts.length > 0 ? posts.map((post) => <PostComponent post={post} posts={posts} setPosts={setPosts} key={post._id}/>) : <p>No posts</p>}
+
+  </div>
+}
