04296d1a29f5b135bec639348e5d82813e4e926b
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-27 16:14:42 +0200

    Created function to get a users information

---
+++ gruppuppgift/src/pages/ProfilePage/getUser.js
@@ -0,0 +1,11 @@
+import { serverUrl } from "../../config";
+
+export async function getUser(username){
+  const response = await fetch(`${serverUrl}/user/${username}`, {
+    credentials: 'include'
+  });
+
+  const user = await response.json();
+
+  return user;
+}
