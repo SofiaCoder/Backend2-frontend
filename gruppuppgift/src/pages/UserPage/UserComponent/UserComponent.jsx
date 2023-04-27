2585334d710d8f590a9d03540e30712008e4b090
Metin Ahmad <metin.ahmad@paligo.net>
2023-04-27 21:17:27 +0200

    Modified structure of Usercomponent

--- gruppuppgift/src/pages/UserPage/UserComponent/UserComponent.jsx
+++ gruppuppgift/src/pages/UserPage/UserComponent/UserComponent.jsx
@@ -1,8 +1,7 @@
 export function UserComponent({user, currentUser}){
   const isFriend = currentUser.friends.includes(user.username);
-return <>
+return <div className="userComponent">
 <span>{user.username}</span>
 <button>{isFriend ? 'Unfollow' : 'Follow'}</button>
-<br />
-</>
+</div>
 }
