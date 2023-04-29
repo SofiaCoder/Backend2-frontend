7434e329dc5e5e67b738042625f1ea3243903171
sofiaCoder <sofia.laas@gmail.com>
2023-04-29 21:36:22 +0200

    Added endpoint to get to different users page

--- gruppuppgift/src/pages/UserPage/UserComponent/UserComponent.jsx
+++ gruppuppgift/src/pages/UserPage/UserComponent/UserComponent.jsx
@@ -1,7 +1,8 @@
+import { Link } from 'react-router-dom'
 export function UserComponent({user, currentUser}){
   const isFriend = currentUser.friends.includes(user.username);
 return <div className="userComponent">
-<span>{user.username}</span>
+<Link to={`/profile/${user.username}`}>{user.username}</Link>
 <button>{isFriend ? 'Unfollow' : 'Follow'}</button>
 </div>
 }
