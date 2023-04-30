e5105a89e8cd3bdec7d570885f2855506fbba488
sofiaCoder <sofia.laas@gmail.com>
2023-04-30 21:28:02 +0200

    Added followUser function onClick

--- gruppuppgift/src/pages/UserPage/UserComponent/UserComponent.jsx
+++ gruppuppgift/src/pages/UserPage/UserComponent/UserComponent.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
+import { followUser } from './followUser';
+
 export function UserComponent({user, currentUser}){
   const isFriend = currentUser.friends.includes(user.username);
 return <div className="userComponent">
-<Link to={`/profile/${user.username}`}>{user.username}</Link>
-<button>{isFriend ? 'Unfollow' : 'Follow'}</button>
+  <Link to={`/profile/${user.username}`}>{user.username}</Link>
+  <button onClick={() => {followUser(user.username)}}>{isFriend ? 'Unfollow' : 'Follow'}</button>
 </div>
 }
