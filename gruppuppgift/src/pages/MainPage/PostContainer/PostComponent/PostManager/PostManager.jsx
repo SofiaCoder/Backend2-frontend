8493c3b641866c1da76dcfa69d2d2fb6e84966cd
Pskantz <112848766+Pskantz@users.noreply.github.com>
2023-05-01 14:28:16 +0200

    editPost, kan du kolla sökvägen annars allt ok

--- gruppuppgift/src/pages/MainPage/PostContainer/PostComponent/PostManager/PostManager.jsx
+++ gruppuppgift/src/pages/MainPage/PostContainer/PostComponent/PostManager/PostManager.jsx
@@ -1,28 +1,41 @@
 import { deletePost } from "./deletePost";
 import "./postManager.css";
+import { editPost } from "./editPost";
 
-export function PostManager({ isEditable, setIsEditable, posts, setPosts, postId, postText }) {
-    function handleSave() {
-        setIsEditable(false);
-    }
+export function PostManager({
+  isEditable,
+  setIsEditable,
+  posts,
+  setPosts,
+  postId,
+  postText,
+}) {
+  async function handleSave() {
+    setIsEditable(false);
+    await editPost(postId, postText);
+  }
 
-    function handleEdit() {
-        setIsEditable(true);
-    }
+  function handleEdit() {
+    setIsEditable(true);
+  }
 
-    async function handleDelete() {
-        await deletePost(postId);
+  async function handleDelete() {
+    await deletePost(postId);
 
-        const clonedPosts = structuredClone(posts);
-        const removalIndex = clonedPosts.findIndex((post) => post._id === postId);
-        clonedPosts.splice(removalIndex, 1);
-        setPosts(clonedPosts);
-    }
-    return (
-        <div className="postManager">
-            {isEditable ? <span onClick={handleSave}>Save</span> : <span onClick={handleEdit}> Edit</span>}
+    const clonedPosts = structuredClone(posts);
+    const removalIndex = clonedPosts.findIndex((post) => post._id === postId);
+    clonedPosts.splice(removalIndex, 1);
+    setPosts(clonedPosts);
+  }
+  return (
+    <div className="postManager">
+      {isEditable ? (
+        <span onClick={handleSave}>Save</span>
+      ) : (
+        <span onClick={handleEdit}> Edit</span>
+      )}
 
-            <span onClick={handleDelete}>Delete</span>
-        </div>
-    );
+      <span onClick={handleDelete}>Delete</span>
+    </div>
+  );
 }
---
+++ gruppuppgift/src/pages/MainPage/PostContainer/PostComponent/PostManager/editPost.js
@@ -0,0 +1,24 @@
+import { serverUrl } from "../../../../..//config";
+
+export async function editPost({ postId, postText }) {
+  try {
+    const response = await fetch(`${serverUrl}/post`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        postID: postId,
+        newPostBody: postText,
+      }),
+    });
+    if (response.ok) {
+      // Refresh the page after the post has been updated
+      window.location.reload();
+    } else {
+      console.log("Failed to update post");
+    }
+  } catch (error) {
+    console.error("Error updating post:", error);
+  }
+}
